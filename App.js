ed1852e1765d1a8ead79fd1e16693c143e5267e6
Alexey Vlasenko <inetgameslam@gmail.com>
2019-11-05 11:17:59 +0200

    react native upgraded to 0.61.4

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './src/lib/store/configureStore';
@@ -8,14 +8,7 @@ const App = () => {
   return (
     <StoreProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <View
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Welcome to RNStarter Project</Text>
-          <Text style={{ textAlign: 'center' }}>
-            Libs: RNavigation 5 (Stack + Tabs), Redux, REanimated (Redash +
-            Retween), GestureHandler
-          </Text>
-        </View>
+        <View />
       </PersistGate>
     </StoreProvider>
   );
