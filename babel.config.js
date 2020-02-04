7aa83c336e6bbf8953ebfba126746318414c76f1
Alexey Vlasenko <inetgameslam@gmail.com>
2020-02-04 15:14:06 +0200

    added: Babel optional chaining, babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@screens': './src/screens',
+          '@constants': './src/lib/constants',
+          '@theme': './src/res/theme',
+          '@navigation': './src/lib/navigators',
+          '@graphql': './src/graphql',
+          '@images': './src/res/images',
+          '@hooks': './src/hooks',
+          '@contexts': './src/contexts',
+          '@components': './src/lib/components',
+          '@res': './src/res',
+          '@actions': './src/store/actions',
+          '@utils': './src/lib/utils',
+          '@api': './src/lib/api',
+        },
+      },
+    ],
+  ],
 };
